95406032c4052f2a500c3df4d9cadbfc898e0e90
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2025-06-16 01:24:33 +0100

    Fix: Update and use 'dark:' directive from TailwindCSS for theme switching.

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -16,11 +16,7 @@ export default function ThemeSwitcher({
   return (
     <button
       onClick={toggleTheme}
-      className={`p-2 rounded-md hover:bg-gray-100 ${
-        darkMode
-          ? "hover:bg-gray-200 hover:text-gray-900"
-          : "hover:bg-gray-900 hover:text-gray-200"
-      } ${className}`}
+      className={`p-2 rounded-md text-gray-900 dark:text-gray-200 dark:hover:bg-gray-200 dark:hover:text-gray-900 hover:bg-gray-900 hover:text-gray-200 ${className}`}
       title={darkMode ? "Switch to light mode" : "Switch to dark mode"}
     >
       {darkMode ? <Sun size={20} /> : <Moon size={20} />}
