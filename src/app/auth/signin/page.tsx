4ff700e6694dab668a076e8626702db4866ccd1a
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2025-07-20 14:17:19 +0100

    Feat: Set up sign in page, use authentication from Auth.js v5 with credentials provider.

---
+++ src/app/auth/signin/page.tsx
@@ -0,0 +1,111 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+import { toast, Toaster } from "react-hot-toast";
+import { signIn } from "next-auth/react";
+
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const credentialsAction = (formData: FormData) => {
+    const username = formData.get("username")?.toString().toLowerCase();
+    const password = formData.get("password")?.toString();
+
+    signIn("credentials", {
+      redirect: true,
+      username,
+      password,
+      redirectTo: "/dashboard",
+    });
+
+    toast.success("Login successful");
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center relative">
+      <div
+        className="absolute inset-0 bg-cover bg-center bg-no-repeat blur-sm"
+        style={{
+          backgroundImage:
+            'url("https://images.unsplash.com/photo-1538481199705-c710c4e965fc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1650&q=80")',
+        }}
+      />
+      <div className="bg-white p-8 rounded-xl shadow-2xl w-full max-w-md relative z-10">
+        <h2 className="text-3xl font-bold text-center mb-8 text-gray-800">
+          Welcome Back!
+        </h2>
+        <form action={credentialsAction} className="space-y-6">
+          <div>
+            <label
+              className="block text-sm font-medium text-gray-700"
+              htmlFor="username"
+            >
+              Username
+            </label>
+            <input
+              type="text"
+              name="username"
+              value={username}
+              placeholder="Username"
+              onChange={(e) => setUsername(e.target.value)}
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 text-gray-600 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+              required
+            />
+          </div>
+          <div>
+            <label
+              className="block text-sm font-medium text-gray-700"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md text-gray-600 shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+              required
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <input
+                type="checkbox"
+                className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+              />
+              <label className="ml-2 block text-sm text-gray-900">
+                Remember me
+              </label>
+            </div>
+            <Link
+              href="/forgot-password"
+              className="text-sm font-medium text-indigo-600 hover:text-indigo-500"
+            >
+              Forgot password?
+            </Link>
+          </div>
+          <button
+            type="submit"
+            className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Sign in
+          </button>
+        </form>
+        <p className="mt-6 text-center text-sm text-gray-600">
+          Don&apos;t have an account?{" "}
+          <Link
+            href="/signup"
+            className="font-medium text-indigo-600 hover:text-indigo-500"
+          >
+            Sign up
+          </Link>
+        </p>
+      </div>
+      <Toaster position="top-center" />
+    </div>
+  );
+}
