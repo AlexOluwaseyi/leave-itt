228f12c9bc1073e18e8fdbb8ca04a61adf45bcd4
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2025-07-28 09:19:40 +0100

    Feat: Wrap AdminDashboard, BookingHistory, Settings, and TeamMembersContent with AccessControlWrapper for role-based access control and URL rewriting.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 import AdminDashboard from "@/components/AdminDashboard";
 import Menu from "@/components/menu/Menu";
+import AccessControlWrapper from "@/components/AccessControlWrapper";
 
 export default function AdminPage() {
   return (
     <Menu>
-      <AdminDashboard />
+      <AccessControlWrapper requiredRoles={["ADMIN", "MANAGER"]}>
+        <AdminDashboard />
+      </AccessControlWrapper>
     </Menu>
   );
 }
